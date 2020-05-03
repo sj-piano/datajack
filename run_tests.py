0198d397d42b2895908a1077302a1a4a6d6b69bf
StJohn Piano <stjohnpiano@gmail.com>
2020-05-03 14:57:32 +0100

    Move tests into datajack dir.

--- run_tests.py
+++ run_tests.py
@@ -3,15 +3,12 @@
 
 import unittest
 
-# Import modules for testing.
-import tests
-
 # Initialize the test suite.
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # Add tests to the test suite.
-suite.addTests(loader.discover('tests'))
+suite.addTests(loader.discover('datajack/tests'))
 
 # Initialize a runner, pass the test suite to it, and run it.
 runner = unittest.TextTestRunner(verbosity=1)
