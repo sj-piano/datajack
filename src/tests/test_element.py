5520ee476574d4015c357d7ff5db78ddea08f71a
StJohn Piano <stjohnpiano@gmail.com>
2020-06-06 14:40:19 +0100

    More tests and fixes for tests.

--- src/tests/test_element.py
+++ src/tests/test_element.py
@@ -44,11 +44,15 @@ class TestNested(unittest.TestCase):
 	
 	def test_getEntryData(self):
 		xpath = 'bar'
-		self.assertEqual(self.e.get(xpath).entryData, 'mars')
+		self.assertEqual(self.e.get(xpath)[0].entryData, 'mars')
 	
 	def test_getValue(self):
 		xpath = 'bar/bas'
-		self.assertEqual(self.e.get(xpath).value, 'ASD')
+		self.assertEqual(self.e.get(xpath)[0].value, 'ASD')
+	
+	def test_getValue2(self):
+		xpath = 'bar/bas'
+		self.assertEqual(self.e.getOne(xpath).value, 'ASD')
 
 
 
@@ -63,17 +67,56 @@ class TestMultiple(unittest.TestCase):
 <item>Orange</item>
 <item>Apple</item>
 <item>Cake</item>
+<sublist>
+<title>Planets</title>
+<planet>1<name>Mercury</name></planet>
+<planet>2<name>Venus</name></planet>
+<planet>3<name>Earth</name></planet>
+<planet>4<name>Mars</name></planet>
+</sublist>
 </list>
 """
 		data = data.strip()
-		logger = createLogger({'name': 'element', 'level': 'debug'})
-		#logger.info('Data: ' + data)
+		logger = createLogger({'name': 'element', 'level': 'error'})
+		logger.info('Data: ' + data)
 		klass.e = Element.fromString(data=data, loggers=[logger])
 	
 	def test_getMultiple(self):
-		xpath = "title"
-		print self.e.get(xpath)
-		#self.assertEqual(self.e.get(xpath), 'Fruit')
+		xpath = "@item"
+		itemValues = [x.value for x in self.e.get(xpath)]
+		expectedValues = 'Orange Apple Cake'.split()
+		self.assertEqual(itemValues, expectedValues)
+	
+	def test_getMultiple2(self):
+		xpath = "sublist/@planet/name"
+		names = [x.value for x in self.e.get(xpath)]
+		expectedNames = "Mercury Venus Earth Mars".split()
+		self.assertEqual(sorted(expectedNames), sorted(names))
+	
+	def test_getMultiple3(self):
+		xpath = "//@name"
+		names = [x.value for x in self.e.get(xpath)]
+		expectedNames = "Mercury Venus Earth Mars".split()
+		self.assertEqual(sorted(expectedNames), sorted(names))
+	
+	def test_getMultipleError(self):
+		xpath = "//@planet"
+		with self.assertRaises(ValueError):
+			self.e.getOne(xpath)
+	
+	def test_getAll(self):
+		xpath = "//@planet"
+		entryDataValues = [x.entryData for x in self.e.getAll(xpath)]
+		expectedValues = "1 2 3 4".split()
+		self.assertEqual(sorted(expectedValues), sorted(entryDataValues))
+	
+	def test_getAllError(self):
+		xpath = "//@planetFOO"
+		with self.assertRaises(ValueError):
+			entryDataValues = [x.entryData for x in self.e.getAll(xpath)]
+
+
+
 
 
 
