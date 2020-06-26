908e41e78be6b3ae6a70fefe61dd8260473de04b
StJohn Piano <stjohnpiano@gmail.com>
2020-06-26 18:44:00 +0100

    Narrow test discovery.

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,7 @@ loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # Add tests to the test suite.
-suite.addTests(loader.discover('src'))
+suite.addTests(loader.discover('src/test'))
 
 # Initialize a runner, pass the test suite to it, and run it.
 runner = unittest.TextTestRunner(verbosity=1)
