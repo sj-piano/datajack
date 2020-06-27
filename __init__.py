f50e830ff41421e1a154145fef8c107c280424be
StJohn Piano <stjohnpiano@gmail.com>
2020-06-27 20:30:45 +0100

    Change location of important __init__.py file.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-# This is the only __init__.py that does any importing.
-# The imports in this file allow an external program to do this:
+# The real importing is done in src/__init__.py.
+# This file allows an external program to do this:
 # import datajack; dj = datajack.Datajack()
-from src.code.Datajack import Datajack
-from src.code.Element import Element
+from src import *
