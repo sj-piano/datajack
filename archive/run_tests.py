08c9e3f118a4272b8317c6d521d5be31f7a9bc16
StJohn Piano <stjohnpiano@gmail.com>
2021-01-09 19:25:44 +0000

    Archive run_tests.py

--- archive/run_tests.py
+++ archive/run_tests.py
@@ -9,6 +9,7 @@ loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # Add tests to the test suite.
+# Note: Test code is run from this file's location, so imports have to be written to run from here as well. 
 suite.addTests(loader.discover('src/test'))
 
 # Initialize a runner, pass the test suite to it, and run it.
