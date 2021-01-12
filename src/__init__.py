b75a3d324797ddf92d757dce4f7c8f82ddf2d625
StJohn Piano <stjohnpiano@gmail.com>
2021-01-12 12:04:48 +0000

    Fix imports and setup.

--- src/__init__.py
+++ src/__init__.py
@@ -5,4 +5,4 @@ from argparse import Namespace
 def setup(args=Namespace()):
 	# Set up the modules in this package.
 	code.setup(args)
-	#util.setup(args)
+	util.setup(args)
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,2 +1,9 @@
+from argparse import Namespace
 import misc, DotDictionary
 import moduleLogger
+import validate
+
+
+def setup(args=Namespace()):
+	# Set up the modules in this package.
+	validate.setup(args)
