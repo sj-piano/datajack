33df9b99df3a891d20804df897c69f78b46f086d
StJohn Piano <stjohnpiano@gmail.com>
2021-03-23 18:30:46 +0000

    tabs -> 2 spaces

--- __init__.py
+++ __init__.py
@@ -22,20 +22,20 @@ deb = logger.debug
 
 
 def setup(a=Namespace()): # a = arguments.
-	a.logger = logger
-	a.loggerName = 'datajack'
-	if 'debug' not in a:
-		a.debug = False
-	if 'logTimestamp' not in a:
-		a.logTimestamp = False
-	if a.debug == True:
-		a.logLevel = 'debug'
-	# Configure logger for this script.
-	src.util.moduleLogger.configureModuleLogger(a)
-	# Configure logging levels for datajack package.
-	# - By default, it does no logging.
-	# - If src.setup() is run without an argument, it has error-level logging.
-	a2 = Namespace(logLevel=a.logLevel, logTimestamp=a.logTimestamp)
-	src.setup(a2)
-	# It's possible to configure logging separately for individual modules, with or without running src.setup().
-	#src.code.Element.setup(Namespace(logLevel='error'))
+  a.logger = logger
+  a.loggerName = 'datajack'
+  if 'debug' not in a:
+    a.debug = False
+  if 'logTimestamp' not in a:
+    a.logTimestamp = False
+  if a.debug == True:
+    a.logLevel = 'debug'
+  # Configure logger for this script.
+  src.util.moduleLogger.configureModuleLogger(a)
+  # Configure logging levels for datajack package.
+  # - By default, it does no logging.
+  # - If src.setup() is run without an argument, it has error-level logging.
+  a2 = Namespace(logLevel=a.logLevel, logTimestamp=a.logTimestamp)
+  src.setup(a2)
+  # It's possible to configure logging separately for individual modules, with or without running src.setup().
+  #src.code.Element.setup(Namespace(logLevel='error'))
--- cli.py
+++ cli.py
@@ -41,80 +41,80 @@ deb = logger.debug
 
 def main():
 
-	parser = argparse.ArgumentParser(
-		description='Command-Line Interface (CLI) for using the datajack package.'
-	)
-	
-	parser.add_argument(
-		'-t', '--task', 
-		help="Task to perform (default: '%(default)s').",
-		default='hello',
-	)	
+  parser = argparse.ArgumentParser(
+    description='Command-Line Interface (CLI) for using the datajack package.'
+  )
 
-	parser.add_argument(
-		'-l', '--logLevel', type=str,
-		choices=['debug', 'info', 'warning', 'error'],
-		help="Choose logging level (default: '%(default)s').",
-		default='info',
-	)
+  parser.add_argument(
+    '-t', '--task',
+    help="Task to perform (default: '%(default)s').",
+    default='hello',
+  )
 
-	parser.add_argument(
-		'-d', '--debug',
-		action='store_true',
-		help="Sets logLevel to 'debug'. This overrides --logLevel.",
-	)
+  parser.add_argument(
+    '-l', '--logLevel', type=str,
+    choices=['debug', 'info', 'warning', 'error'],
+    help="Choose logging level (default: '%(default)s').",
+    default='info',
+  )
 
-	a = parser.parse_args()
+  parser.add_argument(
+    '-d', '--debug',
+    action='store_true',
+    help="Sets logLevel to 'debug'. This overrides --logLevel.",
+  )
 
-	# Setup
-	setup(a)
+  a = parser.parse_args()
 
-	# Run top-level function (i.e. the appropriate task).
-	if a.task == 'hello':
-		hello(a)
-	elif a.task == 'basic':
-		basic(a)
-	else:
-		msg = "Unrecognised task: {}".format(a.task)
-		raise ValueError(msg)
+  # Setup
+  setup(a)
+
+  # Run top-level function (i.e. the appropriate task).
+  if a.task == 'hello':
+    hello(a)
+  elif a.task == 'basic':
+    basic(a)
+  else:
+    msg = "Unrecognised task: {}".format(a.task)
+    raise ValueError(msg)
 
 
 
 
 def setup(a=Namespace()): # a = arguments.
-	a.logger = logger
-	a.loggerName = 'cli'
-	# Configure logger for this script.
-	src.util.moduleLogger.configureModuleLogger(a)
-	# Configure logging levels for datajack package.
-	# - By default, it does no logging.
-	# - If src.setup() is run without an argument, it has error-level logging.
-	src.setup(a)
-	# It's possible to configure logging separately for individual modules, with or without running src.setup().
-	#src.code.Element.setup(Namespace(logLevel='error'))
-	log('Setup complete.')
-	deb('Logger is printing debug output.')
+  a.logger = logger
+  a.loggerName = 'cli'
+  # Configure logger for this script.
+  src.util.moduleLogger.configureModuleLogger(a)
+  # Configure logging levels for datajack package.
+  # - By default, it does no logging.
+  # - If src.setup() is run without an argument, it has error-level logging.
+  src.setup(a)
+  # It's possible to configure logging separately for individual modules, with or without running src.setup().
+  #src.code.Element.setup(Namespace(logLevel='error'))
+  log('Setup complete.')
+  deb('Logger is printing debug output.')
 
 
 
 
 def hello(a):
-	e = Element()
-	assert e.hello() == 'world'
+  e = Element()
+  assert e.hello() == 'world'
 
 
 
 
 def basic(a):
-	input = '<hello>mars</hello>'
-	e = Element.from_string(data=input)
-	assert e.text == 'mars'
+  input = '<hello>mars</hello>'
+  e = Element.from_string(data=input)
+  assert e.text == 'mars'
 
 
 
 
 if __name__ == '__main__':
-	main()
+  main()
 
 
 
