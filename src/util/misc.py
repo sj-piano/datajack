fe8cfc12a7c647ec44957e99e7dcbb1718f84747
StJohn Piano <stjohnpiano@gmail.com>
2021-03-24 10:43:34 +0000

    CamelCase -> snake_case

--- src/util/misc.py
+++ src/util/misc.py
@@ -5,37 +5,37 @@ import inspect
 
 
 def confirm_no_args(args):
-  callerName = inspect.stack()[1][3]
+  caller_name = inspect.stack()[1][3]
   if not isinstance(args, tuple):
     raise TypeError
   if args != ():
-    raise ValueError("Function/method '{c}' doesn't accept positional args.".format(c=callerName))
+    raise ValueError("Function/method '{c}' doesn't accept positional args.".format(c=caller_name))
 
 
 
 
-def get_required_items(dictionary, requiredStr):
-  # Example requiredStr: 'data:s, logger'
+def get_required_items(dictionary, required_str):
+  # Example required_str: 'data:s, logger'
   if not isinstance(dictionary, dict):
     raise TypeError
-  if not isinstance(requiredStr, str):
+  if not isinstance(required_str, str):
     raise TypeError
-  requiredItems = requiredStr.split(', ')
+  required_items = required_str.split(', ')
   results = []
   types = {'i': int, 's': str}
-  for item in requiredItems:
+  for item in required_items:
     if ':' in item:
-      key, valueTypeChar = item.split(':')
-      valueType = types[valueTypeChar]
+      key, value_type_char = item.split(':')
+      value_type = types[value_type_char]
     else:
       key = item
-      valueType = None
+      value_type = None
     if not key in dictionary:
       raise KeyError("Did not find required key: {k}".format(k=key))
     value = dictionary[key]
-    if valueType is not None:
-      if not isinstance(value, valueType):
-        msg = "Dictionary item '{k}' is not a '{t}', as was specified. Instead, it is a '{t2}'.".format(k=key, t=valueType.__name__, t2=type(value).__name__)
+    if value_type is not None:
+      if not isinstance(value, value_type):
+        msg = "Dictionary item '{k}' is not a '{t}', as was specified. Instead, it is a '{t2}'.".format(k=key, t=value_type.__name__, t2=type(value).__name__)
         raise TypeError(msg)
     results.append(value)
   if len(results) == 1:
@@ -45,32 +45,32 @@ def get_required_items(dictionary, requiredStr):
 
 
 
-def get_optional_items(dictionary, optionalStr, defaults):
-  # Example optionalStr: 'parent, line:i, lineIndex:i'
+def get_optional_items(dictionary, optional_str, defaults):
+  # Example optional_str: 'parent, line:i, lineIndex:i'
   if not isinstance(dictionary, dict):
     raise TypeError
-  if not isinstance(optionalStr, str):
+  if not isinstance(optional_str, str):
     raise TypeError
   if not isinstance(defaults, tuple):
     raise TypeError
-  optionalItems = optionalStr.split(', ')
+  optional_items = optional_str.split(', ')
   results = []
   types = {'i': int, 's': str}
-  for i, item in enumerate(optionalItems):
+  for i, item in enumerate(optional_items):
     default = defaults[i]
     if ':' in item:
-      key, valueTypeChar = item.split(':')
-      valueType = types[valueTypeChar]
+      key, value_type_char = item.split(':')
+      value_type = types[value_type_char]
     else:
       key = item
-      valueType = None
+      value_type = None
     if key in dictionary:
       value = dictionary[key]
     else:
       value = default
-    if valueType is not None:
-      if not isinstance(value, valueType):
-        msg = "Optional dictionary item '{k}' found but is not a '{t}', as was specified. Instead, it is a '{t2}'.".format(k=key, t=valueType.__name__, t2=type(value).__name__)
+    if value_type is not None:
+      if not isinstance(value, value_type):
+        msg = "Optional dictionary item '{k}' found but is not a '{t}', as was specified. Instead, it is a '{t2}'.".format(k=key, t=value_type.__name__, t2=type(value).__name__)
         raise TypeError(msg)
     results.append(value)
   if len(results) == 1:
