3f46d81ed3a5df2cb0096cbfa5ebf7224c3e3f07
StJohn Piano <stjohnpiano@gmail.com>
2021-04-22 15:50:55 +0100

    Rewrite setup()

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 # Imports
-from argparse import Namespace
 import logging
 
 
@@ -12,7 +11,7 @@ from . import datajack
 
 
 # Collect up the things that we want in the immediate namespace of the imported datajack module.
-# E.g. datajack.Element()
+# So that from outside this package we can do e.g. datajack.Element()
 Element = datajack.code.Element.Element
 Entry = datajack.code.Element.Entry
 validate = datajack.util.validate
@@ -24,28 +23,34 @@ configure_module_logger = datajack.util.module_logger.configure_module_logger
 # Set up logger for this module. By default, it produces no output.
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.ERROR)
 log = logger.info
 deb = logger.debug
 
 
 
 
-def setup(a=Namespace()):  # a = arguments.
-  a.logger = logger
-  a.loggerName = 'datajack'
-  if 'debug' not in a:
-    a.debug = False
-  if 'logTimestamp' not in a:
-    a.logTimestamp = False
-  if a.debug == True:
-    a.logLevel = 'debug'
-  # Configure logger for this script.
-  datajack.util.module_logger.configure_module_logger(a)
-  # Configure logging levels for datajack package.
-  # - By default, it does no logging.
-  # - If datajack.setup() is run without an argument, it has error-level logging.
-  a2 = Namespace(logLevel=a.logLevel, logTimestamp=a.logTimestamp)
-  datajack.setup(a2)
-  # It's possible to configure logging separately for individual modules, with or without running datajack.setup().
-  #datajack.code.Element.setup(Namespace(logLevel='error'))
-
+def setup(
+    log_level = 'error',
+    debug = False,
+    log_timestamp = False,
+    log_filepath = None,
+    ):
+  # Configure logger for this module.
+  datajack.util.module_logger.configure_module_logger(
+    logger = logger,
+    logger_name = __name__,
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
+  log('Setup complete.')
+  deb('Logger is logging at debug level.')
+  # Configure modules further down in this package.
+  datajack.setup(
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
