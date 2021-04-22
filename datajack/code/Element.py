4846de20aef2089a4e9dfb01e55208df76b89d3a
StJohn Piano <stjohnpiano@gmail.com>
2021-04-22 15:51:41 +0100

    Expand setup()

--- datajack/code/Element.py
+++ datajack/code/Element.py
@@ -1,6 +1,5 @@
 # Imports
 import logging
-import argparse
 import string
 
 
@@ -13,7 +12,6 @@ from .. import util
 
 
 # Shortcuts
-Namespace = argparse.Namespace
 v = util.validate
 
 
@@ -22,16 +20,30 @@ v = util.validate
 # Set up logger for this module. By default, it produces no output.
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.ERROR)
 log = logger.info
 deb = logger.debug
 
 
 
 
-def setup(args=Namespace()):
-  args.logger = logger
+def setup(
+    log_level = 'error',
+    debug = False,
+    log_timestamp = False,
+    log_filepath = None,
+    ):
   # Configure logger for this module.
-  util.module_logger.configure_module_logger(args)
+  util.module_logger.configure_module_logger(
+    logger = logger,
+    logger_name = __name__,
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
+  log('Setup complete.')
+  deb('Logger is logging at debug level.')
 
 
 
