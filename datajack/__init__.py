9ed119ab0df49153ed616ee7232070320d9f418d
StJohn Piano <stjohnpiano@gmail.com>
2021-04-22 15:51:06 +0100

    Expand setup()

--- datajack/__init__.py
+++ datajack/__init__.py
@@ -1,5 +1,5 @@
 # Imports
-from argparse import Namespace
+import logging
 
 
 
@@ -11,8 +11,37 @@ from . import util
 
 
 
-def setup(args=Namespace()):
-  # Set up the modules in this package.
-  code.setup(args)
-  util.setup(args)
+# Set up logger for this module. By default, it produces no output.
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.ERROR)
+log = logger.info
+deb = logger.debug
 
+
+
+
+def setup(
+    log_level = 'error',
+    debug = False,
+    log_timestamp = False,
+    log_filepath = None,
+    ):
+  # Configure logger for this module.
+  util.module_logger.configure_module_logger(
+    logger = logger,
+    logger_name = __name__,
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
+  log('Setup complete.')
+  deb('Logger is logging at debug level.')
+  # Configure modules further down in this package.
+  code.setup(
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
